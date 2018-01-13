58a4ad9fae6366d8a511f0a6f8dbf722a01a521a
Ashwin Natarajan <ashwinn3@gmail.com>
2018-01-13 17:30:03 -0500

    documentation for API access

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -8,5 +8,9 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+        System.out.println("\nDocumentation: ");
+        System.out.println("\nPOST request example for register: " + "localhost:8080/register?username=test1@password=324823");
+        System.out.println("\nGET request example for login (return success/error): " + "localhost:8080/login?username=test1&password=2384u23");
+
     }
 }
