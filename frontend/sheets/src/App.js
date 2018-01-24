0a34f13dc687138d6c4d1ebe2eb50236ebb2c5a3
Niklas Dorsch <niklas@evidentid.com>
2018-01-24 14:44:20 -0500

    added an old mock frontend I did

--- frontend/sheets/src/App.js
+++ frontend/sheets/src/App.js
@@ -1,19 +1,92 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
 import './App.css';
+import 'bulma/css/bulma.css';
+
+import Toolbar from './Toolbar.js';
+import LoginPage from './LoginPage.js';
+import NotFound from './NotFound.js';
+import LoginSwitch from './LoginSwitch.js';
+
+
+const RedirectHome = class extends Component {
+  render() {
+    return <Redirect to='/'  />;
+  }
+}
+const Home = class extends Component {
+  render() {
+    return <div>You are home</div>
+  }
+}
+
+
 
 class App extends Component {
+  constructor(...args) {
+    super(...args);
+    this.handelLogin = this.handelLogin.bind(this);
+
+    this.state = {
+      id: "",
+      name: "",
+      requestFailed : false,
+      loggedIn : false
+
+    };
+  }
+
+  componentDidMount() {
+
+  }
+
+  handelLogin(info) {
+    this.setState(
+      { loggedIn : info.login,
+        id       : info.id,
+        name     : info.name}
+    )
+  }
+  handelLogout = () => {
+    this.setState(
+      {loggedIn : false,
+       id       : null}
+    )
+  }
+
   render() {
+    var elements = null;
+    let userInfo = {id : this.state.id,
+                    name : this.state.name}
+    if (this.state.loggedIn) {
+      elements = (
+          <div>
+            <Toolbar handleLogout={this.handelLogout} userName={this.state.name} userID={this.state.id}/>
+            <Switch>
+              <Route exact path="/" render={(props) => (
+                <Home {...props} userInfo={userInfo}/>)} />
+              <Route path="/login" component={RedirectHome}/>
+              <Route path="*" component={NotFound}/>
+            </Switch>
+          </div>
+            );
+    } else {
+      elements =  <Switch>
+                    <Route exact path="/login" render={(props) => (
+                        <LoginPage {...props} method={this.handelLogin} />)} />
+                    <Route component={LoginSwitch}/>
+                  </Switch>;
+    }
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <BrowserRouter>
+
+        <div className="App">
+            {elements}
+
+        </div>
+     </BrowserRouter>
+
     );
   }
 }
